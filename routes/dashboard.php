9670c6cb6fad63cd9363b69572d38dec3173cdf6
MostafaAlii <mostafa0alii@gmail.com>
2022-08-29 04:46:35 +0200

    set admin login design & change from vite to mix

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -5,9 +5,11 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'auth']
     ], function(){
         Route::prefix('Dashboard')->group(function() {
-            Route::get('/', Dashboard\DashboardController::class)->name('dashboard.index');
+            Route::get('/', Dashboard\DashboardController::class)->name('dashboard');
         });
+        /******************************** End Other Authentication Route ****************** */
 });
+require __DIR__.'/auth.php';
